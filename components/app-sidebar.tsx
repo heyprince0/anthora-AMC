d7bc5c555978bf7853d1a23c497ef8fa04bd5a47
Prince <itsprince139@gmail.com>
2026-08-09 22:50:13 +0530

    Remove 'Service Alerts' from technician navigation

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -57,7 +57,6 @@ const adminOnlyNavItems = [
 ]
 
 const technicianNavItems = [
-  { title: "Service Alerts", icon: Bell, href: "/alerts" },
   { title: "Service History", icon: History, href: "/history" },
 ]
 
